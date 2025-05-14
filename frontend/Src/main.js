396c504aa85c8a1b1ed93bb1de9691a4c4425bf4
Jassy1233 <meseguer318@gmail.com>
2025-05-14 21:55:03 +0200

    Eliminado el scroll y los botones

--- frontend/Src/main.js
+++ frontend/Src/main.js
@@ -1,20 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Slide de los botones : 
-
-    //Scroll del indicador
-    document.getElementById('scrollBtn1').addEventListener('click', function () {
-        document.getElementById('sec1').scrollIntoView({ behavior: 'smooth' });
-    })
-
-    // Scroll de la seccion 2 :
-    // document.getElementById('scrollBtn2').addEventListener('click', function () {
-    //     document.getElementById('sec2').scrollIntoView({ behavior: 'smooth' });
-    //  });
-
-    // Scroll de la seccion 3 :
-    document.getElementById('scrollBtn3').addEventListener('click', function () {
-        document.getElementById('sec3').scrollIntoView({ behavior: 'smooth' })
-    })
 
     // Modo oscuro : 
 
